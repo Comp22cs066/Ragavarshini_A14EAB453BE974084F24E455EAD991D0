bc91f8744f8e8a1f112f42769cf41b17fb9ad3a8
Comp22cs066 <144256427+Comp22cs066@users.noreply.github.com>
2023-09-14 21:09:44 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,20 +1,13 @@
-def isLeapYear(year):
-  if(year%4==0 and year%100!=0) or year%400==0:
-    return True
-  else:
-    return False 
-
-year=int(input("Enter the year:"))
-
-if isLeapYear(year):
-  print(year,"is a leap year")
-else:
-  print(year,"is not a leap year")
-
-
-
-
-
-
-
-
+class player:
+  def play(self):
+    print("The player is playing ")
+class batsman(player):
+  def play(self):
+    super().play()
+    print("The batsman is batting")
+class bowler(batsman):
+  def play(self):
+    super().play()
+    print("The bowler is bowling")
+s=bowler()
+s.play()
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
-
-
-number = int(input("Enter the value:"))
-res = fact_rec(number)
-
-print("The factorial of", number, "is", res)
+class BankAccount:
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      # self.__account_balance = self.__account_balance+amount
+      print("Deposited ₹{}. New balance: ₹{}".format(amount,
+self.__account_balance))
+    else:
+      print("Invalid deposit amount.")
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      # self.__account_balance = self.__account_balance - amount
+      print("Withdrew ₹{}. New balance: ₹{}".format(amount,
+                                                    self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
