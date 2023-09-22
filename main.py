c6fd61c9ef36e4eb46aa1c1253b29e2d1bbdbf78
Comp22cs066 <144256427+Comp22cs066@users.noreply.github.com>
2023-09-22 22:40:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-class BankAccount:
-  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
-  def deposit(self, amount):
-    if amount > 0:
-      self.__account_balance += amount
-      # self.__account_balance = self.__account_balance+amount
-      print("Deposited ₹{}. New balance: ₹{}".format(amount,
-self.__account_balance))
-    else:
-      print("Invalid deposit amount.")
-  def withdraw(self, amount):
-    if amount > 0 and amount <= self.__account_balance:
-      self.__account_balance -= amount
-      # self.__account_balance = self.__account_balance - amount
-      print("Withdrew ₹{}. New balance: ₹{}".format(amount,
-                                                    self.__account_balance))
-    else:
-      print("Invalid withdrawal amount or insufficient balance.")
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+result = linearSearchProduct(products, target)
+print(result)
